157f887066f70b2f9fced48c675fd602dadb61f1
Jesse Mortenson <jessemortenson@gmail.com>
2015-02-28 20:56:55 -0600

    Initial commit of scrapy project

---
+++ showoff_scrape/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for showoff_scrape project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'showoff_scrape'
+
+SPIDER_MODULES = ['showoff_scrape.spiders']
+NEWSPIDER_MODULE = 'showoff_scrape.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'showoff_scrape (+http://www.yourdomain.com)'
