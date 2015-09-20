c05c0ed4f0f3875536c60e2a45393ad10a4b6a13
Jesse MOrtenson <jessemortenson@gmail.com>
2015-09-20 02:17:12 -0500

    Changing pipeline to point to new CreateShowbill endpoint in engine

--- showoff_scrape/pipelines.py
+++ showoff_scrape/pipelines.py
@@ -27,17 +27,18 @@ class ShowoffScrapePipeline(object):
         item_json = jsonpickle.encode(item['showbill'], unpicklable=False)
 
         # Make API call to save item
-        event_endpoint = 'http://127.0.0.1:8000/events/'
+        event_endpoint = 'http://web/showbill/'
         log.msg("item_json: " + item_json, level=log.DEBUG)
-        # event_response = requests.post(event_endpoint, data=item_json)
-        #
-        # # does this event already exist?
-        # if event_response.status_code == 409:
-        #     raise DropItem("Existing ShowBill found: %s" % item.discovery_section.found_url)
-        #
-        # elif event_response.status_code == requests.codes.created:
-        #     return item
-        #
-        # else:
-        #     # we got an unexpected status code
-        #     event_response.raise_for_status()
+        event_response = requests.post(event_endpoint, data=item_json)
+        log.msg('event_response: ' + str(event_response.status_code), level=log.DEBUG)
+        
+        # does this event already exist?
+        if event_response.status_code == 409:
+            raise DropItem("Existing ShowBill found: %s" % item.discovery_section.found_url)
+        
+        elif event_response.status_code == requests.codes.created:
+            return item
+        
+        else:
+            # we got an unexpected status code
+            event_response.raise_for_status()
