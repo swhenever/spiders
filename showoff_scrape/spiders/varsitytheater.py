742876bb3c7e7e9cf835aee6695958dd773a96ec
Jesse Mortenson <jessemortenson@gmail.com>
2016-07-08 01:10:10 -0500

    Second draft of Varsity Theater spider

--- showoff_scrape/spiders/varsitytheater.py
+++ showoff_scrape/spiders/varsitytheater.py
@@ -36,6 +36,30 @@ class VarsitySpider(CrawlSpider):
     def kill_unicode_and_strip(self, text):
         return re.sub(r'[^\x00-\x7f]',r'',text).strip()
 
+    # Return correct arrow time matching pattern for the given time string
+    def make_arrow_time_pattern(self, time):
+        if re.search(r':', time):
+            time_pattern = 'h:mma'
+        else:
+            time_pattern = 'ha'
+        return time_pattern
+
+    # Process a string that may contain performers and return array of performers
+    def process_performers_string(self, performers_string, force_split=False):
+        # get rid of "blahblah presents" or "Presented by blah blah"
+        performers_string = re.sub(r'^.+\Wpresents\W|\Wpresented by.+$', r'', performers_string, 0, re.IGNORECASE)
+
+        # if we are forcing a split, or find text that indicates to  us a comma-separated list
+        if force_split or re.search(r'with special guests|featuring', performers_string, re.IGNORECASE):
+            # get rid of the unnecessary text
+            performers_string = re.sub(r'^.+\Wwith special guests\W|^.+?\W?featuring\W', r'', performers_string, 0, re.IGNORECASE)
+            performers = re.split(r', |, AND | AND |AND ', performers_string)
+            performers = filter(None, performers)  # eliminate empty strings
+            performers = map(lambda p: self.kill_unicode_and_strip(p), performers) # sanitize/strip each value
+        else:
+            performers = [self.kill_unicode_and_strip(performers_string)]
+        return performers
+
     def parse_show(self, response):
         #inspect_response(response, self)
 
@@ -54,9 +78,9 @@ class VarsitySpider(CrawlSpider):
 
         # Most properties are in a set of LIs
         property_text_strings = response.css('article.cpt_events ul.album-meta li::text').extract()
-        date_string = self.kill_unicode_and_strip(property_text_strings[0])
-        ticket_price_string = self.kill_unicode_and_strip(property_text_strings[1])
-        time_ages_string = self.kill_unicode_and_strip(property_text_strings[2])
+        date_string = self.kill_unicode_and_strip(property_text_strings[0])  # 22 - Jul - 2016
+        ticket_price_string = self.kill_unicode_and_strip(property_text_strings[1])  # $20 Advance / $25 Day Of Show
+        time_ages_string = self.kill_unicode_and_strip(property_text_strings[2])  # 7pm Doors - 8pm Music - 16+ with I.D.
 
         # ticket prices
         # string is like: $18 Advance / $20 Day of show
@@ -80,30 +104,46 @@ class VarsitySpider(CrawlSpider):
 
         # age restriction
         # TODO START HERE - age restriction is in time_ages_string - "7pm Doors - 8pm Music - 18+ with I.D."
-        event_section.minimumAgeRestriction = 0
+
+        ages = re.findall(ur'(\d+\+|All Ages)', time_ages_string)
+        if len(ages) > 0 and ages[0] == 'All Ages':
+            event_section.minimumAgeRestriction = 0
+        elif len(ages) > 0:
+            event_section.minimumAgeRestriction = ages[0].strip(' +')
 
         # parse doors date/time
-        doors_string = response.css('div.event-info h2.times span.doors::text').extract()  # Doors: 7:00 pm
-        doors_string = self.kill_unicode_and_strip(doors_string[0])
-        doors_string = re.search(ur'\d+:\d+ [ap]m', doors_string).group(0)  # 7:00 pm
-        start_string = response.css('div.event-info h2.times span.start::text').extract()  # Show: 8:00 pm
-        start_string = self.kill_unicode_and_strip(start_string[0])
-        start_string = re.search(ur'\d+:\d+ [ap]m', start_string).group(0)  # 8:00 pm
-        date_string = response.css('div.event-info h2.dates::text').extract()  # Fri, July 15, 2016
-        date_string = self.kill_unicode_and_strip(date_string[0])
-
-        doors_date = arrow.get(date_string + doors_string, [r"\w+, MMMM +D, YYYYh:mm a"], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))
-        event_section.doorsDatetime = doors_date
-        start_date = arrow.get(date_string + start_string, [r"\w+, MMMM +D, YYYYh:mm a"], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))
-        event_section.startDatetime = start_date
+        # 22 - Jul - 20167pm
+        times = re.findall(ur'\d?\d[ap]m|\d?\d:\d{2}[ap]m', time_ages_string)
+        if len(times) > 1:
+            doors_date = arrow.get(date_string + times[0], [r"DD - MMM - YYYY" + self.make_arrow_time_pattern(times[0])], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))
+            event_section.doorsDatetime = doors_date
+            start_date = arrow.get(date_string + times[1], [r"DD - MMM - YYYY" + self.make_arrow_time_pattern(times[1])], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))
+            event_section.startDatetime = start_date
+        else:
+            start_date = arrow.get(date_string + times[0], [r"DD - MMM - YYYY" + self.make_arrow_time_pattern(times[0])], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))
+            event_section.startDatetime = start_date
 
         # PERFORMERS SECTION
         # find performers
-        performerStrings = response.css('div.artist-boxes div.artist-headline span.artist-name::text').extract()
+        headline_string_raw = response.css('article.cpt_events h1.entry-title::text').extract()
+
+        # this sometimes has an unicode em dash in it. If so, split string by that
+        # we're using a stupid hack to accomplish this (zap all unicode into an ascii placeholder)
+        headline_string_raw = re.sub(r'[^\x00-\x7f]', r'|+|', headline_string_raw[0])
+        performer_name_strings = headline_string_raw.split('|+|')
+
+        # add any special guests to the list of performer names
+        guest_string_raw = response.css('article.cpt_events p:last::text').extract()
+        if len(guest_string_raw) > 0:
+            guest_string = self.kill_unicode_and_strip(guest_string_raw[0])
+            if not re.search(r'TBA', guest_string, re.IGNORECASE):
+                guest_performances = self.process_performers_string(guest_string)
+                performer_name_strings = performer_name_strings + guest_performances
+
         performances = []
-        for i, performer in enumerate(performerStrings):
+        for i, performer in enumerate(performer_name_strings):
             performance_section = PerformanceSection()
-            performance_section.name = self.kill_unicode_and_strip(performer)
+            performance_section.name = performer
             performance_section.order = i
             performances.append(performance_section)
 
