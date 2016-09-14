15695a176c84c91876ab93129ee7942b109509a8
Jesse Mortenson <jessemortenson@gmail.com>
2016-09-14 15:50:16 -0500

    Finished icehouse refactoring, added to showspiderutils

--- showoff_scrape/spiders/icehouse.py
+++ showoff_scrape/spiders/icehouse.py
@@ -41,22 +41,24 @@ class IcehouseSpider(CrawlSpider):
         # EVENT SECTION
         event_section = EventSection()
 
+        # lots of meta content in h2 tags in body
+        body_h2_strings = response.css('.eventitem-column-content h2::text').extract()
+
         # date/time
         # Some shows have a separate doors/showtime listing in body, some just have the header area start time
         # Check first for a doors/showtime listing
-        body_h2_strings = response.css('.eventitem-column-content h2::text').extract()
         start_time_string = doors_time_string = None
         for h2_string_index, h2_string_text in enumerate(body_h2_strings):
             # 3 groups in regex, so this returns a list of tuples
             times = re.findall(ur'(\d+(:\d\d)?[p|a]m\s(doors|show))', h2_string_text, re.IGNORECASE)
             if len(times) == 1:
-                start_time_string = showspiderutils.kill_unicode_and_strip(times[0][0]).replace(' doors', '').replace(' show', '')
+                start_time_string = showspiderutils.time_normalize(showspiderutils.kill_unicode_and_strip(times[0][0]).replace(' doors', '').replace(' show', ''))
                 doors_time_string = None
             elif len(times) == 2:
                 # first is doors
-                doors_time_string = showspiderutils.kill_unicode_and_strip(times[0][0]).strip(' doors')  # 10:30pm or 4pm
+                doors_time_string = showspiderutils.time_normalize(showspiderutils.kill_unicode_and_strip(times[0][0]).strip(' doors'))  # 10:30pm or 4pm
                 # second is showtime, if present
-                start_time_string = showspiderutils.kill_unicode_and_strip(times[1][0]).strip(' show')
+                start_time_string = showspiderutils.time_normalize(showspiderutils.kill_unicode_and_strip(times[1][0]).strip(' show'))
 
         # fallback to header area start time if couldn't find the above
         if start_time_string is None:
@@ -64,49 +66,61 @@ class IcehouseSpider(CrawlSpider):
             start_time_string = response.css('p.event-time time.event-time-12hr::text').extract()
             start_time_string = showspiderutils.kill_unicode_and_strip(start_time_string)
             times = re.findall(ur'\d+:\d\d[p|a]m', start_time_string, re.IGNORECASE)
-            start_time_string = times[0]
-
-        # handle if time is "4pm" instead of "4:00pm"
-        # @todo
+            start_time_string = showspiderutils.time_normalize(times[0])
 
         date_string = response.css('p.event-time time.event-meta-heading::text').extract()
         date_string = showspiderutils.kill_unicode_and_strip(date_string[0])
         event_section.startDatetime = arrow.get(start_time_string.strip() + " " + date_string.strip(), [r"h:mma \w+, MMMM +D, YYYY"], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))  # 11:00pm Friday, July  8, 2016
         if doors_time_string is not None:
-            event_section.endDatetime = arrow.get(doors_time_string.strip() + " " + date_string.strip(), [r"h:mma \w+, MMMM +D, YYYY"], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))  # 11:00pm Friday, July  8, 2016
+            event_section.doorsDatetime = arrow.get(doors_time_string.strip() + " " + date_string.strip(), [r"h:mma \w+, MMMM +D, YYYY"], locale='en').replace(tzinfo=dateutil.tz.gettz(self.timezone))  # 11:00pm Friday, July  8, 2016
 
-        # @todo START HERE
         # title / event url (and performers)
-        title_selector = response.css('h1.entry-title')
-        performers_text = title_selector.css('a::text').extract()  # MARK MALLMAN + RONiiA
-        performers_text = showspiderutils.kill_unicode_and_strip(performers_text[0])
-        event_section.title = performers_text
-        event_url = title_selector.css('a::attr(href)').extract()
-        event_section.eventUrl = venue_section.venueUrl + event_url[0]
+        title_string = response.css('h1.page-title::text').extract()
+        event_section.title = performers_combined_string = showspiderutils.kill_unicode_and_strip(title_string[0])
+        event_section.eventUrl = response.url
 
         # ticket price
-        possible_price_strings = response.css('div.eventlist-excerpt h2::text').extract()
-        for price_index, possible_price in enumerate(possible_price_strings):
-            if re.search('no cover', possible_price, re.IGNORECASE):
-                event_section.ticketPriceDoors = 0
-            elif re.search('at the door', possible_price, re.IGNORECASE):
-                prices = re.findall(ur'[$]\d+(?:\.\d{2})?', possible_price)
-                if len(prices) == 2:
-                    event_section.ticketPriceAdvance = float(prices[0].strip('$'))
-                    event_section.ticketPriceDoors = float(prices[1].strip('$'))
-                elif len(prices) == 1:
-                    event_section.ticketPriceDoors = float(prices[0].strip('$'))
+        for price_index, possible_price in enumerate(body_h2_strings):
+            prices = showspiderutils.check_text_for_prices(possible_price)
+            if prices['doors'] is not None:
+                event_section.ticketPriceDoors = prices['doors']
+            if prices['advance'] is not None:
+                event_section.ticketPriceAdvance = prices['advance']
 
-        # @todo: ticket URL. Would need to change to a multiple level spider, because this is in the event page
+        # get ticket URLs. Have to check every A tag in the body for BUY TICKETS text :(
+        possible_tickets = response.css('.eventitem-column-content a')
+        possible_ticket_texts = possible_tickets.xpath('text()').extract()
+        possible_ticket_urls = possible_tickets.xpath('@href').extract()
+        for ticket_url_index, possible_ticket_url_text in enumerate(possible_ticket_texts):
+            if re.search(ur'buy tickets', possible_ticket_url_text, re.IGNORECASE):
+                event_section.ticketPurchaseUrl = possible_ticket_urls[ticket_url_index]
+
+        # age restriction
+        for age_index, possible_age_text in enumerate(body_h2_strings):
+            minimum_age = showspiderutils.check_text_for_age_restriction(possible_age_text)
+            if minimum_age is not None:
+                event_section.minimumAgeRestriction = minimum_age
 
         # performances
         performances = []
-        performers = performers_text.split(' + ')
-        for performer_index, performer in enumerate(performers):
+        performer_strings = response.css('.eventitem-column-content h3::text').extract()
+        for performer_index, performer in enumerate(performer_strings):
             performance_section = PerformanceSection()
             performance_section.name = showspiderutils.kill_unicode_and_strip(performer)
+            # often a colon is at the end of the performer name
+            if performance_section.name.endswith(":"):
+                performance_section.name = performance_section.name[:-1]
             performance_section.order = performer_index
             performances.append(performance_section)
+
+        if len(performances) == 0:
+            # fallback to the performers combined string in the title
+            performers = performers_combined_string.split(' + ')
+            for performer_index, performer in enumerate(performers):
+                performance_section = PerformanceSection()
+                performance_section.name = showspiderutils.kill_unicode_and_strip(performer)
+                performance_section.order = performer_index
+                performances.append(performance_section)
 
         # MAKE HipLiveMusicShowBill
         showbill = HipLiveMusicShowBill(discovery_section, venue_section, event_section, performances)
--- showoff_scrape/spiders/showspiderutils.py
+++ showoff_scrape/spiders/showspiderutils.py
@@ -1,6 +1,6 @@
 import scrapy
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 import arrow
 import dateutil
 import re
@@ -10,6 +10,19 @@ from showoff_scrape.items import *
 
 def kill_unicode_and_strip(text):
     return re.sub(r'[^\x00-\x7f]', r'', text).strip()
+
+
+# Normalize time string to 4:30pm format
+def time_normalize(time_string):
+    am_or_pm = re.search(ur'(am|pm)', time_string, re.IGNORECASE)
+    if am_or_pm:
+        am_or_pm = am_or_pm.group(0)
+    else:
+        am_or_pm = 'pm'  # assume pm
+    time = re.sub('[^0-9:]', '', time_string)  # leave only numbers, colon
+    if not re.search(r':', time):
+        time += ':00'  # assume it is something like "4" or "12" and just add minutes
+    return time + am_or_pm
 
 
 def make_discovery_section(script_name):
@@ -37,3 +50,20 @@ def check_text_for_age_restriction(subject_text):
         return int(re.search(r'\d\d\+', subject_text, re.IGNORECASE).group().strip('+'))
     elif re.search(r'\d\d and over|\d\d & over', subject_text, re.IGNORECASE):
         return int(re.search(r'(\d\d)(?: and over)|(\d\d)(?: & over)', subject_text, re.IGNORECASE).groups()[0])
+    else:
+        return None
+
+
+def check_text_for_prices(subject_text):
+    prices = {'doors': None, 'advance': None}
+    if re.search(ur'no cover|free', subject_text, re.IGNORECASE):
+        prices['doors'] = 0
+    else:
+        prices_found = re.findall(ur'[$]\d+(?:\.\d{2})?', subject_text)
+        if len(prices_found) == 2:
+            # assume that door price is listed first
+            prices['advance'] = float(prices_found[0].strip('$'))
+            prices['doors'] = float(prices_found[1].strip('$'))
+        elif len(prices_found) == 1:
+            prices['doors'] = float(prices_found[0].strip('$'))
+    return prices
